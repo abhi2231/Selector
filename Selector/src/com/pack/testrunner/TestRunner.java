79c02806488f1e972c3e53e387eaa859e2fb4d7e
Nilesh <pasarate@gmail.com>
2017-07-06 16:23:55 +0530

    serialized/deserialized implemented....

--- Selector/src/com/pack/testrunner/TestRunner.java
+++ Selector/src/com/pack/testrunner/TestRunner.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -13,14 +14,21 @@ import org.testng.IMethodInterceptor;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
-public class TestRunner implements IMethodInterceptor {
+import com.pack.serialize.ObjectWareHouse;
+
+public class TestRunner implements IMethodInterceptor, Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public transient List<IMethodInstance> result;
+	public ObjectWareHouse owh;
+	public String path = null;
 
 	@Override
 	public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
 
 		int test_Start_Index = 0, test_End_Index = 0;
 		FileReader fis = null;
-		String startstr = null, endstr = null, path = null;
+		String startstr = null, endstr = null;
 
 		path = System.getProperty("user.dir");
 		File file = new File(path + "\\src\\com\\pack\\resource\\config.properties");
@@ -33,7 +41,6 @@ public class TestRunner implements IMethodInterceptor {
 		try {
 			prop.load(fis);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		startstr = prop.getProperty("Test_Start_Index");
@@ -43,24 +50,39 @@ public class TestRunner implements IMethodInterceptor {
 		System.out.println("===============================================");
 		System.out.println("Test_Start_Count = " + test_Start_Index + " , " + "Test_End_Count = " + test_End_Index
 				+ " , Total Tests Selected = " + (test_End_Index - test_Start_Index + 1));
-		System.out.println("===============================================");
-		List<IMethodInstance> result = new ArrayList<IMethodInstance>();
+		System.out.println("===============================================\n");
+		result = new ArrayList<IMethodInstance>();
 
 		for (IMethodInstance method : methods) {
-
 			Test testmethod = method.getMethod().getConstructorOrMethod().getMethod().getAnnotation(Test.class);
-
 			int priority = testmethod.priority();
-
 			if (priority >= test_Start_Index && priority <= test_End_Index) {
-
 				result.add(method);
-
 			}
 		}
-
+		serializedObjectState();
+		try {
+			deserialezedObjectState();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("===============================================\n");
+		System.out.println("Below Test Cases executed...\n");
 		return result;
 
+	}
+
+	public void serializedObjectState() {
+		owh = new ObjectWareHouse(result, path);
+		try {
+			owh.saveObjectState();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deserialezedObjectState() throws ClassNotFoundException, IOException {
+		owh.readObjectState();
 	}
 
 }
